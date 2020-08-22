f43ad0aede014ecc07f914cb94711123f18e72f7
likes linux but uses windows <46166499+SBSProgrammer@users.noreply.github.com>
2020-08-22 12:41:37 +0200

    sorry, but the "overhaul" ended up being smaller than i expected
    yeah, couldnt realy convert it into a switch case... sadly, i feel like theres no choice but to say welcome to yanderedev style code

--- Main.cpp
+++ Main.cpp
@@ -1,40 +1,50 @@
 #include "GeneralUtils.h"
 
-int main() {
-	string answer;
+int main()
+{
+	std::string answer;
 
 	GeneralUtils::print("Think of a number between 2 and 8, and I will try and guess it for you.");
 	GeneralUtils::print("Only answer \"Higher\", \"Equal\" or \"Lower\". All words are case-sensitive.");
 	GeneralUtils::print("Is this number a 5?");
 
 	answer = GeneralUtils::input();
-	if (answer == "Higher") {
+	if (answer == "Higher")
+	{
 		GeneralUtils::print("Is this number a 7?");
 		answer = GeneralUtils::input();
-		if (answer == "Higher") {
+		if (answer == "Higher")
+		{
 			GeneralUtils::print("This number is a 8! Am I right?");
 		}
-		else if (answer == "Lower") {
+		else if (answer == "Lower")
+		{
 			GeneralUtils::print("This number is a 6! Am I right?");
 		}
-		else if (answer == "Equal") {
+		else if (answer == "Equal")
+		{
 			GeneralUtils::print("I know, this number is a 7!");
 		}
 	}
-	else if (answer == "Lower") {
+	else if (answer == "Lower")
+	{
 		GeneralUtils::print("Is this number a 3?");
 		answer = GeneralUtils::input();
-		if (answer == "Higher") {
+		if (answer == "Higher")
+		{
 			GeneralUtils::print("This number is a 4! Am I right?");
 		}
-		else if (answer == "Lower") {
+		else if (answer == "Lower")
+		{
 			GeneralUtils::print("This number is a 2! Am I right?");
 		}
-		else if (answer == "Equal") {
+		else if (answer == "Equal")
+		{
 			GeneralUtils::print("I know, this number is a 3!");
 		}
 	}
-	else if (answer == "Equal") {
+	else if (answer == "Equal")
+	{
 		GeneralUtils::print("I know, this number is a 5!");
 	}
 
